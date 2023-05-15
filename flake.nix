8fb37264819d178aefc13746c3493b6ea2b9451c
Po-Yeh Chen <poyehchen@cs.nctu.edu.tw>
2023-05-15 20:42:20 +0800

    Add nodevenv for rust.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
       path = ./rust/binary;
       description = "A simple Rust binary project.";
     };
+    templates.rust-binary-nodevenv = {
+      path = ./rust/no-devenv/binary;
+      description = "A simple Rust binary project.";
+    };
 
     templates.default = self.templates.rust-binary;
   };
