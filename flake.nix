433b01828beed1ed693483f2c9647e6e71cf26db
Po-Yeh Chen <poyehchen@cs.nctu.edu.tw>
2023-05-15 20:38:38 +0800

    Revert "Add no-devenv options."
    This reverts commit 035dee7ca245e92ed99d629ab6b1de7dec74bae1.

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
       path = ./rust/binary;
       description = "A simple Rust binary project.";
     };
-    templates.rust-binary-nodevenv = {
-      path = ./rust/no-devenv/binary;
-      description = "A simple Rust binary project without devenv.";
-    };
 
     templates.default = self.templates.rust-binary;
   };
